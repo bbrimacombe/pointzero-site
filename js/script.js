9b356d2c21cb38e458938e8f8b33c96444941778
Abdelhady Salah <hadysalah1455@gmail.com>
2022-02-19 03:17:00 +0200

    Add google recaptcha to email form

--- js/script.js
+++ js/script.js
@@ -152,26 +152,30 @@ ${translation.rightSide}`
 	
 	$('#mc-embedded-subscribe-form').submit((e) => {
 		e.preventDefault()
-		const form = $('#mc-embedded-subscribe-form')
-		$.ajax({
-			type: 'GET',
-			contentType: 'application/json',
-			url: form.attr('action'),
-			data: form.serialize(),
-			dataType: 'jsonp',
-			jsonp: 'c',
-			cache: false,
-			error: (e) => console.log(e),
-			success: (data) => {
-				if (data.result === 'success') {
-					$('#mc-update-message').css({ display: 'block', color: 'green' })
-					$('#mc-embedded-subscribe-form').css({ display: 'none' })
-					$('#mc-update-message').html('Thank you for subscribing!')
-				} else {
-					$('#mc-update-message').css({ display: 'block', color: 'red' })
-					$('#mc-update-message').html(data.msg.includes('already subscribed') === true ? 'You are already subscribed.' : 'Failed to subscribe due to an error. Please try again later.')
-				}
-			}
-		})
+		grecaptcha.ready(function() {
+			grecaptcha.execute(grecaptchaSiteKey, {action: 'submit'}).then(function(token) {
+				const form = $('#mc-embedded-subscribe-form')
+				$.ajax({
+					type: 'GET',
+					contentType: 'application/json',
+					url: form.attr('action'),
+					data: form.serialize(),
+					dataType: 'jsonp',
+					jsonp: 'c',
+					cache: false,
+					error: (e) => console.log(e),
+					success: (data) => {
+						if (data.result === 'success') {
+							$('#mc-update-message').css({ display: 'block', color: 'green' })
+							$('#mc-embedded-subscribe-form').css({ display: 'none' })
+							$('#mc-update-message').html('Thank you for subscribing!')
+						} else {
+							$('#mc-update-message').css({ display: 'block', color: 'red' })
+							$('#mc-update-message').html(data.msg.includes('already subscribed') === true ? 'You are already subscribed.' : 'Failed to subscribe due to an error. Please try again later.')
+						}
+					}
+				})
+			});
+		});
 	})
 });
