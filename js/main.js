4ac851543108cb3d42e88d23f1c7787d6f50362a
Abdelhady Salah <hadysalah1455@gmail.com>
2022-02-20 20:20:15 +0200

    Include uploadCode in JS module

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@ import './langs.js'
 import './samples/main.js'
 import './translate/main.js'
 import './mailchimpSubscribe.js'
+import './uploadCode.js'
 
 export const grecaptchaSiteKey = '6LcMNoUeAAAAAKCDfuNh0u9r1ZqgpjyZ0s2cTTn2'
