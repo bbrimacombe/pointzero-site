12f7f828033ad2dbc28f65ca43f35961099bef88
Abdelhady Salah <hadysalah1455@gmail.com>
2022-02-22 21:57:04 +0200

    Provide sampleData to reloadSamples

--- js/samples/onLangChange.js
+++ js/samples/onLangChange.js
@@ -1,8 +1,9 @@
 import { langs } from '../langs.js'
 import { inputEditor } from '../codemirror.js'
 import { reloadSamples } from './reloadSamples.js'
+import { sampleData } from './sampleData.js'
 
 export const onLangChange = (fromLang) => {
     inputEditor.setOption('mode', langs.filter((lang) => lang.name === fromLang)[0].mode)
-    reloadSamples()
+    reloadSamples(sampleData)
 }
