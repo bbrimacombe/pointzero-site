ce35926bce6944bd2c763e393380a0f8055e431b
Abdelhady Salah <hadysalah1455@gmail.com>
2022-02-22 04:11:36 +0200

    Load codemirror by device width

--- js/codemirror.js
+++ js/codemirror.js
@@ -8,6 +8,34 @@ const codeMirrorConfig = {
 	theme: 'yonce',
 }
 
+/* ***************
+<div class="column-right" id="outputContainer">
+    <div style="width: 100%">
+    <textarea id="outputText" class="codemirror-textarea">Output Code Here</textarea>
+    </div>
+</div>
+   *************** */
+
+const width = window.innerWidth
+const isMobile = width <= 600 ? true : false
+const targetOutputElement = isMobile === true ? document.querySelector('#outputMobile') : document.querySelector('#outputDesktop')
+const docFrag = document.createDocumentFragment()
+const columnRightDiv = document.createElement('div')
+
+columnRightDiv.classList.add('column-right')
+columnRightDiv.id = 'outputContainer'
+docFrag.appendChild(columnRightDiv)
+
+const innerDiv = document.createElement('div')
+innerDiv.style.width = '100%'
+columnRightDiv.appendChild(innerDiv)
+
+const outputTextArea = document.createElement('textarea')
+outputTextArea.id = 'outputText'
+outputTextArea.classList.add('codemirror-textarea')
+innerDiv.appendChild(outputTextArea)
+targetOutputElement.appendChild(docFrag)
+
 export const inputEditor = CodeMirror.fromTextArea(document.querySelector('#inputText'), {
 	...codeMirrorConfig,
 	mode: 'text'
