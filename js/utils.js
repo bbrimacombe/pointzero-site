cdb1fd2e3e2d79a9b7031272c5cdcc59b3eda645
Abdelhady Salah <hadysalah1455@gmail.com>
2022-02-22 21:55:01 +0200

    Add getRandomInt

--- js/utils.js
+++ js/utils.js
@@ -16,3 +16,10 @@ export const rankTextRelevance = (searchString, string) => {
 	})
 	return rank
 }
+
+// Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+export const getRandomInt = (min, max) => {
+	min = Math.ceil(min)
+	max = Math.floor(max)
+	return Math.floor(Math.random() * (max - min) + min)
+  }
