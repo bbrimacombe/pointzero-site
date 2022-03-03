05edac156ab9d0251a715f6186215bcb064d0ffe
Abdelhady Salah <hadysalah1455@gmail.com>
2022-03-03 03:15:09 +0200

    Implement reCAPTCHA verification

--- js/main.js
+++ js/main.js
@@ -5,4 +5,17 @@ import './mailchimpSubscribe.js'
 import './uploadCode.js'
 
 export const grecaptchaSiteKey = '6LcMNoUeAAAAAKCDfuNh0u9r1ZqgpjyZ0s2cTTn2'
+export const grecaptchaSecretKey = '6LcMNoUeAAAAAGLn6u86pAQt5BreUHQwFA4EYYyg'
+const scoreLimit = 6.0
 export const showSampleOutput = false
+
+export async function verifyReCaptchaV3(token) {
+    let isHuman = false
+    try {
+        const res = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${grecaptchaSecretKey}&response=${token}`)
+        console.log(res.data)
+        isHuman =  res.data.success
+    } catch (e) {
+        console.log(e.response)
+    }
+}
--- js/translate/translateTextAction.js
+++ js/translate/translateTextAction.js
@@ -1,13 +1,15 @@
 import { inputEditor, outputEditor } from '../codemirror.js'
 import { translateCode } from './translateCode.js'
-import { grecaptchaSiteKey } from '../main.js'
+import { grecaptchaSiteKey, verifyReCaptchaV3 } from '../main.js'
 
 export const translateTextAction = (e) => {
 	e.preventDefault();
 	if (document.querySelector('#fromLang').value === 'text') return alert('Please select a language to translate from')
 	if (document.querySelector('#toLang').value === 'text') return alert('Please select a language to translate to')
 	grecaptcha.ready(function() {
-		grecaptcha.execute(grecaptchaSiteKey, {action: 'submit'}).then(async function(token) {
+		grecaptcha.execute(grecaptchaSiteKey, {action: 'translateTextAction'}).then(async function(token) {
+			let isHuman = verifyReCaptchaV3(token)
+			if (!isHuman) return alert('Request verification failed')
 			const leftSide = inputEditor.getValue()
 			const rightSide = outputEditor.getValue()
 			outputEditor.setValue('Waiting...')
